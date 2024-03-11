8935d4326949fe7926ffbadd7b0657aa5af64118
Alexander M <fh.marinenko_sasha@mail.ru>
2024-03-12 00:34:38 +0300

    Рабочая версия

--- src/test/java/tests/AutomationPracticeFormTest.java
+++ src/test/java/tests/AutomationPracticeFormTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -26,7 +27,7 @@ public class AutomationPracticeFormTest {
         $("#firstName").setValue("Sasha");
         $("#lastName").setValue("Mario");
         $("#userEmail").setValue("email@mail.ru");
-        $("#userNumber").setValue("88005553535");
+        $("#userNumber").setValue("8800555353");
         $(".custom-radio").click();
         $(byText("Music")).click();
         $("#currentAddress").setValue("www leningrad");
@@ -34,12 +35,32 @@ public class AutomationPracticeFormTest {
         //Выбор объекта
         $(By.id("subjectsInput")).setValue("M").pressEnter();
 
-        //Импорт картинки (!!)
+        //Загрузка
         $("#uploadPicture").uploadFromClasspath("images/test2.png");
 
+        //Государство и город
+        $("#state").click();
+        $(byText("NCR")).click();
+        $("#city").click();
+        $(byText("Delhi")).click();
 
+        //День рождения
+        $("#dateOfBirthInput").click();
+        $(By.className("react-datepicker__day--001")).doubleClick();
 
         $("#submit").click();
+
+        //Проверка формы
+        $(".table-responsive").shouldHave(text("Sasha Mario")) ;
+        $(".table-responsive").shouldHave(text("email@mail.ru")) ;
+        $(".table-responsive").shouldHave(text("8800555353")) ;
+        $(".table-responsive").shouldHave(text("Male")) ;
+        $(".table-responsive").shouldHave(text("Music")) ;
+        $(".table-responsive").shouldHave(text("01 March,2024")) ;
+        $(".table-responsive").shouldHave(text("Maths")) ;
+        $(".table-responsive").shouldHave(text("test2.png")) ;
+        $(".table-responsive").shouldHave(text("www leningrad")) ;
+        $(".table-responsive").shouldHave(text("NCR Delhi")) ;
 
     }
 }
