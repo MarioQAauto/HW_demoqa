571733037a07b1411d6c0b189988073257a54aa0
Alexander M <fh.marinenko_sasha@mail.ru>
2024-03-11 21:21:27 +0300

    rename and delete stuff

--- src/test/java/tests/automationPracticeForm.java
+++ src/test/java/tests/automationPracticeForm.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TextBoxTests {
+public class automationPracticeForm {
 
     @BeforeAll
     static void beforeAll() {
